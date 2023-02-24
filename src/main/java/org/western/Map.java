72d290094fc6a6fe1712739a7a383d65df3cd97f
365cent <m@mx.sb>
2023-02-24 11:00:40 -0500

    feat: create Map class

---
+++ src/main/java/org/western/Map.java
@@ -0,0 +1,19 @@
+package org.western;
+
+public class Map {
+    private Building[] buildings;
+    private String[] layers;
+
+    public Map(Building[] buildings, String[] layers) {
+        this.buildings = buildings;
+        this.layers = layers;
+    }
+
+    public Building[] getBuildings() {
+        return buildings;
+    }
+
+    public String[] getLayers() {
+        return layers;
+    }
+}
