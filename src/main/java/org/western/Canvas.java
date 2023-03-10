15eff55e94b6b6a6c635040f4a67ec51ea40efc5
jding263 <jding263@uwo.ca>
2023-03-10 13:45:05 -0500

    fix: Add mouse press event listener to make initial location of map correct

--- src/main/java/org/western/Canvas.java
+++ src/main/java/org/western/Canvas.java
@@ -2,21 +2,25 @@ package org.western;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
+import java.util.Objects;
 
-// http://www.java2s.com/Code/Java/2D-Graphics-GUI/Imagewithmousedragandmoveevent.htm
+// Modified and add mouse press event listener to make initial location correct
+// Adapted from http://www.java2s.com/Code/Java/2D-Graphics-GUI/Imagewithmousedragandmoveevent.htm
 
 class Canvas extends JPanel {
-    int x, y;
+    int x, y, initialX, initialY;
     BufferedImage bi;
 
     Canvas(String f, int w, int h) {
         setBackground(Color.white);
         setSize(w, h);
+        addMouseListener(new MouseHandler());
         addMouseMotionListener(new MouseMotionHandler());
-        Image image = new ImageIcon(getClass().getResource(f)).getImage();
+        Image image = new ImageIcon(Objects.requireNonNull(getClass().getResource(f))).getImage();
         // scale image to fit window
         image = image.getScaledInstance(w, image.getHeight(this) * w / image.getWidth(this), Image.SCALE_SMOOTH);
 
@@ -46,11 +50,28 @@ class Canvas extends JPanel {
         g2D.drawImage(bi, x, y, this);
     }
 
+    class MouseHandler extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            initialX = e.getX();
+            initialY = e.getY();
+        }
+    }
+
     class MouseMotionHandler extends MouseMotionAdapter {
         public void mouseDragged(MouseEvent e) {
-            x = e.getX();
-            y = e.getY();
+            int currentX = e.getX();
+            int currentY = e.getY();
+            int deltaX = currentX - initialX;
+            int deltaY = currentY - initialY;
+
+            x += deltaX;
+            y += deltaY;
+
+            initialX = currentX;
+            initialY = currentY;
+
             repaint();
         }
     }
+
 }
