d3f38d0332dbf23d61b9b3779718e26ade13861e
jding263 <jding263@uwo.ca>
2023-03-13 19:32:28 -0400

    fix[db]: Replace deprecated function with the latest syntax

--- src/main/java/org/western/JsonDB.java
+++ src/main/java/org/western/JsonDB.java
@@ -1,39 +1,40 @@
 package org.western;
 
 import java.io.FileReader;
+import java.util.Objects;
 
 import com.google.gson.*;
 
 public class JsonDB {
     private JsonObject data;
     public JsonDB(String field, String value) {
-        int i = 0;
+        int i;
         JsonObject j;
         Gson g = new Gson();
         if(field.equals("user") || field.equals("poi")) {
-            try (FileReader f = new FileReader(getClass().getResource("/org/western/db/" + field + ".json").getFile())) { // new file reader
-                j = new JsonParser().parse(f).getAsJsonObject(); // parse json file
+            try (FileReader f = new FileReader(Objects.requireNonNull(getClass().getResource("/org/western/db/" + field + ".json")).getFile())) { // new file reader
+                j = JsonParser.parseReader(f).getAsJsonObject(); // parse json file
                 if(field.equals("user"))
                 {
                     i = j.get("data").getAsJsonObject().get(value).getAsInt();
                     if(i < j.get("count").getAsInt()) { // check if user exists
-                        try (FileReader r = new FileReader(getClass().getResource("/org/western/db/" + i + ".json").getFile())) { // new file reader
+                        try (FileReader r = new FileReader(Objects.requireNonNull(getClass().getResource("/org/western/db/" + i + ".json")).getFile())) { // new file reader
                             User u = g.fromJson(r, User.class); // convert to json element
                             data = g.toJsonTree(u).getAsJsonObject(); // convert to json object
                         }
                     } else {
-                        data = new JsonParser().parse("{\"status\": 500, \"message\": \"Internal Error\"}").getAsJsonObject(); // internal error
+                        data = JsonParser.parseString("{\"status\": 500, \"message\": \"Internal Error\"}").getAsJsonObject(); // internal error
                     }
                 } else {
                     POI p = g.fromJson(f, POI.class); // convert to json element
                     data = g.toJsonTree(p).getAsJsonObject(); // convert to json object
                 }
             } catch (Exception e) {
-                data = new JsonParser().parse("{\"status\": 404, \"message\": \"Not Found\"}").getAsJsonObject(); // not found
+                data = JsonParser.parseString("{\"status\": 404, \"message\": \"Not Found\"}").getAsJsonObject(); // not found
                 e.printStackTrace();
             }
         } else {
-            data = new JsonParser().parse("{\"status\": 400, \"message\": \"Invalid field\"}").getAsJsonObject(); // invalid field
+            data = JsonParser.parseString("{\"status\": 400, \"message\": \"Invalid field\"}").getAsJsonObject(); // invalid field
         }
     }
     public JsonObject getData() {
