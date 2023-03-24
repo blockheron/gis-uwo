0219cf87a6cf2c8b643b9381e8fdd64a747f2200
jding263 <jding263@uwo.ca>
2023-03-24 11:44:07 -0400

    chore: Modify mock data

--- src/main/java/org/western/POI.java
+++ src/main/java/org/western/POI.java
@@ -5,15 +5,20 @@ public class POI {
     private String name;
     private String description;
     private String building;
+    private String floor;
+
+    private String layer;
     private String room;
     private float[][] coordinates = new float[4][2];
     private int[] user;
 
-    public POI(int id, String name, String description, String building, String room, float[][] coordinates, int[] user) {
+    public POI(int id, String name, String description, String building, String floor, String layer, String room, float[][] coordinates, int[] user) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.building = building;
+        this.floor = floor;
+        this.layer = layer;
         this.room = room;
         this.coordinates = coordinates;
         this.user = user;
@@ -23,21 +28,35 @@ public class POI {
     public int getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public String getBuilding() {
         return building;
     }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public String getLayer() {
+        return layer;
+    }
+
     public String getRoom() {
         return room;
     }
+
     public float[][] getCoordinates() {
         return coordinates;
     }
+
     public int[] getUser() {
         return user;
     }
