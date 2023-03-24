e7f1899961ad977a3f3268ba39d4238b3278c6b6
jding263 <jding263@uwo.ca>
2023-03-24 11:53:01 -0400

    BREAKING CHANGE: Comment out map class

--- src/main/java/org/western/Map.java
+++ src/main/java/org/western/Map.java
@@ -1,19 +1,21 @@
-package org.western;
+//package org.western;
+//
+//public class Map {
+//    private Building[] buildings;
+//    private String[] layers;
+//
+//    public Map(Building[] buildings, String[] layers) {
+//        this.buildings = buildings;
+//        this.layers = layers;
+//    }
+//
+//    public Building[] getBuildings() {
+//        return buildings;
+//    }
+//
+//    public String[] getLayers() {
+//        return layers;
+//    }
+//}
 
-public class Map {
-    private Building[] buildings;
-    private String[] layers;
-
-    public Map(Building[] buildings, String[] layers) {
-        this.buildings = buildings;
-        this.layers = layers;
-    }
-
-    public Building[] getBuildings() {
-        return buildings;
-    }
-
-    public String[] getLayers() {
-        return layers;
-    }
-}
+// Pending deletion
