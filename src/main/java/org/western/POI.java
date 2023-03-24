52a29e1ec926a4c6003d83963abd5a68caa49cd8
jding263 <jding263@uwo.ca>
2023-03-24 11:34:30 -0400

    BREAKING CHANGE: Update POI class structure

--- src/main/java/org/western/POI.java
+++ src/main/java/org/western/POI.java
@@ -4,45 +4,41 @@ public class POI {
     private int id;
     private String name;
     private String description;
+    private String building;
     private String room;
     private float[][] coordinates = new float[4][2];
-    private Building building;
-    private User user;
+    private int[] user;
 
-    public POI(int id, String name, String description, Building building, String room, float[][] coordinates) {
+    public POI(int id, String name, String description, String building, String room, float[][] coordinates, int[] user) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.building = building;
         this.room = room;
         this.coordinates = coordinates;
+        this.user = user;
     }
+
 
     public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
-
     public String getDescription() {
         return description;
     }
-
+    public String getBuilding() {
+        return building;
+    }
     public String getRoom() {
         return room;
     }
-
     public float[][] getCoordinates() {
         return coordinates;
     }
-
-    public Building getBuilding() {
-        return building;
-    }
-
-    public User getUser() {
+    public int[] getUser() {
         return user;
     }
 }
